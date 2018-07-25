169f3ac403d267cf133843ef9a6ab69e4d972556
fulanin <matheusrotondano@hotmail.com>
2018-07-25 17:40:22 -0300

    user name and email modeling

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  before_save {self.email = self.email.downcase}
+  validates :name, presence: true, 
+                   length: {maximum: 64}
+  validates :email, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i},
+                    uniqueness: {case_sensitive: false}
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180723161138) do
+ActiveRecord::Schema.define(version: 20180725202924) do
 
   create_table "foos", force: :cascade do |t|
     t.string "bar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
