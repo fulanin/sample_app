1a6c7fca5bb87bb86937a160fd06ce1b080a4420
fulanin <matheusrotondano@hotmail.com>
2018-07-30 16:59:34 -0300

    finished user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
                    length: {maximum: 64}
   validates :email, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i},
                     uniqueness: {case_sensitive: false}
+  validates :password, presence: true, 
+                       length: {minimum: 6}
   has_secure_password
 end
