62ead04fc2159daf34db267e8ef268547f35e00e
fulanin <matheusrotondano@hotmail.com>
2018-08-03 15:19:30 -0300

    implemented advanced login with remember me

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,15 +6,43 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # Remembers a user in a persistent session
+  def remember(user)
+    user.remember
+
+    # The cookies method is treated as a hash, which consists in a value and an expires fields
+    # Setting a cookie that expires in 20.years.from_now is so common in rails that the cookies.pormanent method already does that
+    # The .signed method encrypts the cookie before placing it on the browser
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Forgets a persistent session
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+
   # Logs out the current user
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
 
   # Returns the current logged-in user (if any)
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if (user && user.authenticated?(cookies[:remember_token]))
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # Returns true if the user is logged in or false otherwise
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save {self.email = self.email.downcase}
   validates :name, presence: true, 
                    length: {maximum: 64}
@@ -7,4 +8,39 @@ class User < ApplicationRecord
   validates :password, presence: true, 
                        length: {minimum: 6}
   has_secure_password
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers logged in user in the database
+  def remember
+    self.remember_token = User.new_token
+
+    # remember_token se refere ao que foi definido utilizando attr_acessor
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Does the opposite of remember
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  # Verifies if the given remember_token matches the user's remember digest
+  # Returns true if the given token matches the digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+
+    # remember_token refers to the parameter on the function
+    # BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    BCrypt::Password.new(remember_digest) == remember_token
+  end
 end
